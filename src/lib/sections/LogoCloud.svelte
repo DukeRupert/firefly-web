f46a1a034ad4d632106c136bd2df863868a02c39
Logan Williams <logan@firefly.llc>
2022-07-17 17:34:24 -0600

    Created Features, LogoCloud and CTA components

---
+++ src/lib/sections/LogoCloud.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	// Todo
+</script>
+
+<!-- This example requires Tailwind CSS v2.0+ -->
+<div class="relative bg-white py-16 sm:py-24 lg:py-32">
+	<div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+		<p class="text-center text-base font-semibold uppercase text-gray-600 tracking-wider">
+			Trusted by over 5 very average small businesses
+		</p>
+		<div class="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
+			<div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+				<img
+					class="h-12"
+					src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg"
+					alt="Tuple"
+				/>
+			</div>
+			<div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+				<img
+					class="h-12"
+					src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg"
+					alt="Mirage"
+				/>
+			</div>
+			<div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
+				<img
+					class="h-12"
+					src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg"
+					alt="StaticKit"
+				/>
+			</div>
+			<div class="col-span-1 flex justify-center md:col-span-3 lg:col-span-1">
+				<img
+					class="h-12"
+					src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg"
+					alt="Transistor"
+				/>
+			</div>
+			<div class="col-span-2 flex justify-center md:col-span-3 lg:col-span-1">
+				<img
+					class="h-12"
+					src="https://tailwindui.com/img/logos/workcation-logo-gray-400.svg"
+					alt="Workcation"
+				/>
+			</div>
+		</div>
+	</div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
+	import Cta from '$lib/sections/CTA.svelte';
+	import Features from '$lib/sections/Features.svelte';
 	import Hero from '$lib/sections/Hero.svelte';
+	import LogoCloud from '$lib/sections/LogoCloud.svelte';
 </script>
 
 <main>
 	<Hero />
+	<Features />
+	<Cta />
+	<LogoCloud />
 </main>
