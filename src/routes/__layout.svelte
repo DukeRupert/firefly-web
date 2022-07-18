932d34db5505a71ee9917da2f58c80f5a9a8465f
Logan Williams <logan@firefly.llc>
2022-07-17 20:52:26 -0600

    Added Footer widget

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import '../app.css';
 	import Navbar from '$lib/widgets/navbar/index.svelte';
+	import Footer from '$lib/widgets/footer/index.svelte';
 </script>
 
 <div class="relative overflow-hidden">
 	<Navbar />
 	<slot />
+	<Footer />
 </div>
