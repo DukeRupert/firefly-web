91e34c672445d91614d0be8e6cc8f4cf7b99b5ab
Logan Williams <logan@firefly.llc>
2022-07-19 22:54:29 -0600

    Carousel component for logo cloud created.

---
+++ src/lib/components/Carousel.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+
+	interface Logo {
+		src: string;
+		alt: string;
+	}
+
+	export let logos: Logo[];
+
+	let index = 0;
+
+	const next = () => {
+		index = (index + 1) % logos.length;
+	};
+</script>
+
+<!-- markup (zero or more items) goes here -->
+{#each [logos[index]] as { src, alt } (index)}
+	<img on:click={next} transition:fade {src} {alt} class="absolute h-32 w-64 object-contain" />
+{/each}
--- src/lib/sections/LogoCloud.svelte
+++ src/lib/sections/LogoCloud.svelte
@@ -1,49 +1,26 @@
 <script lang="ts">
-	// Todo
+	import Carousel from '$lib/components/Carousel.svelte';
+	interface Logo {
+		src: string;
+		alt: string;
+	}
+	const logos: Logo[] = [
+		{ src: '/logo-kcc.svg', alt: 'Kagen Coffee & Crepes logo' },
+		{ src: '/logo-osc.png', alt: 'One Sleep Company logo' },
+		{ src: '/logo-fts.png', alt: 'FtS Excavation logo' },
+		{ src: '/logo-trbc.jpg', alt: 'The Ridge Bible Church logo' },
+		{ src: '/logo-ag.webp', alt: 'Aestus cRPG Guides logo' }
+	];
 </script>
 
 <!-- This example requires Tailwind CSS v2.0+ -->
 <div class="relative bg-white py-16 sm:py-24 lg:py-32">
 	<div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
 		<p class="text-center text-base font-semibold uppercase text-gray-600 tracking-wider">
-			Trusted by over 5 very average small businesses
+			Proudly trusted by many small businesses
 		</p>
-		<div class="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
-			<div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-				<img
-					class="h-12"
-					src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg"
-					alt="Tuple"
-				/>
-			</div>
-			<div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-				<img
-					class="h-12"
-					src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg"
-					alt="Mirage"
-				/>
-			</div>
-			<div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-				<img
-					class="h-12"
-					src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg"
-					alt="StaticKit"
-				/>
-			</div>
-			<div class="col-span-1 flex justify-center md:col-span-3 lg:col-span-1">
-				<img
-					class="h-12"
-					src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg"
-					alt="Transistor"
-				/>
-			</div>
-			<div class="col-span-2 flex justify-center md:col-span-3 lg:col-span-1">
-				<img
-					class="h-12"
-					src="https://tailwindui.com/img/logos/workcation-logo-gray-400.svg"
-					alt="Workcation"
-				/>
-			</div>
+		<div class="mt-8 h-32 w-64 relative flex mx-auto">
+			<Carousel {logos} />
 		</div>
 	</div>
 </div>
