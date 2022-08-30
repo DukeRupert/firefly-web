40b2c3a90450def3c3d7056035cae5d8d8f920d5
Logan Williams <logan@firefly.llc>
2022-08-30 17:42:44 -0600

    Hero component only reacts to user scroll and changes at 1 tenth screen height change.

--- src/lib/sections/Hero.svelte
+++ src/lib/sections/Hero.svelte
@@ -1,51 +1,28 @@
 <script lang="ts">
 	// innerWidth and innerHeight
-	let width = 0;
 	let height = 0;
 
 	// Scroll Y position
 	let y: number;
 
-	// Mouse position
-	let m = { x: 0, y: 0 };
-
-	// Update mouse position
-	function handleMousemove(event: MouseEvent) {
-		m.x = event.clientX;
-		m.y = event.clientY;
-	}
-
-	// Set day to false when mouse is on the right half of the screen
-	function changeBackgroundOnMouse(x: number, width: number) {
-		let half = Math.round(width / 2);
-		let day = x < half ? true : false;
-		return day;
-	}
-
-	// Set day to false when user scrolls past 1/5 height
+	// Set day to false when user scrolls past 1/10 height
 	function changeBackgroundOnScroll(y: number, height: number) {
-		let day = y > height ? false : true;
+		let tenth = Math.round(height / 10);
+		let day = y > tenth ? false : true;
 		return day;
 	}
 
-	// Set day to false if either Mouse or Scroll is false
-	function changeBackground(x: number, y: number, width: number, height: number) {
-		const mouse = changeBackgroundOnMouse(x, width);
-		const scroll = changeBackgroundOnScroll(y, height);
-		return !mouse || !scroll ? false : true;
-	}
-
-	$: day = changeBackground(m.x, y, width, 200);
+	$: day = changeBackgroundOnScroll(y, height);
 </script>
 
-<svelte:window bind:scrollY={y} bind:innerWidth={width} bind:innerHeight={height} />
+<svelte:window bind:scrollY={y} bind:innerHeight={height} />
 
 <div
 	class="fixed -z-10 top-0 left-0 w-screen h-screen bg-gray-900 {day
 		? "bg-[url('/backgrounds/Valley-day.svg')]"
 		: "bg-[url('/backgrounds/Valley-night.svg')]"} bg-cover bg-center transition-all  ease-in duration-500"
 />
-<div class="mx-auto max-w-6xl" on:mousemove={handleMousemove}>
+<div class="mx-auto max-w-6xl">
 	<div
 		class="mt-[30vh] mb-[50vh] mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8"
 	>
